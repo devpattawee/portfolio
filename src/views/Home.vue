1c3a728072d2256454afee998e309ebf590d31b4
devpattawee <dev.pattawee@gmail.com>
2021-01-09 21:15:02 +0700

    initial

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <Navbar/>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import Navbar from '@/components/structure/Navbar.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+    Navbar
+  }
+}
+</script>
